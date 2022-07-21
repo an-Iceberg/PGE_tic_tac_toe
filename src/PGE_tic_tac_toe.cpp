499059510aafbe1499d59cd0de791bb967d8a3d9
an-Iceberg <a.g.marosi@bluewin.ch>
2022-07-21 15:18:33 +0200

    added PvE/PvP UI element

--- src/PGE_tic_tac_toe.cpp
+++ src/PGE_tic_tac_toe.cpp
@@ -8,6 +8,12 @@ enum cellContent
   CIRCLE
 };
 
+enum playMode
+{
+  PvE,
+  PvP
+};
+
 struct cell
 {
   olc::vi2d spritePosition;
@@ -33,12 +39,18 @@ private:
   olc::Sprite *circle;
   int width;
   int height;
-  int oneThird;
+  int oneThird; // The distance for one third of the grid or the position of the second cell from the top left of the grid
   int twoThirds;
+  int offset; // This makes the cross and circle centered
+  cellContent player1Sprite;
+  playMode mode;
   olc::vi2d mouse;
   cell cells [9];
 
+  olc::Pixel orange = olc::Pixel(255, 128, 0);
+
   // The field width/height is the same as the window height
+  // Formula for height/distance relation: y = 6x + 94
 
 public:
   PGE_tic_tac_toe()
@@ -55,29 +67,39 @@ public:
     oneThird = height * 0.33;
     twoThirds = oneThird * 2;
 
+    // Calculating the offset by modifying the above formula to x = (y - 94) / 6
+    offset = (height + 1 - 94) / 6;
+
     // The sprites are 32x32px
     cross = new olc::Sprite("./sprites/cross.png");
     circle = new olc::Sprite("./sprites/circle.png");
 
-    // TODO: find a position that centers the sprites
-    cells[0].spritePosition = olc::vi2d{1, 1};
-    cells[1].spritePosition = olc::vi2d{oneThird + 2, 1};
-    cells[2].spritePosition = olc::vi2d{twoThirds + 2, 1};
+    cells[0].spritePosition = olc::vi2d{offset, offset};
+    cells[1].spritePosition = olc::vi2d{oneThird + 1 + offset, offset};
+    cells[2].spritePosition = olc::vi2d{twoThirds + 2 + offset, offset};
 
-    cells[3].spritePosition = olc::vi2d{1, oneThird + 2};
-    cells[4].spritePosition = olc::vi2d{oneThird + 2, oneThird + 2};
-    cells[5].spritePosition = olc::vi2d{twoThirds + 2, oneThird + 2};
+    cells[3].spritePosition = olc::vi2d{offset, oneThird + 1 + offset};
+    cells[4].spritePosition = olc::vi2d{oneThird + 1 + offset, oneThird + 1 + offset};
+    cells[5].spritePosition = olc::vi2d{twoThirds + 2 + offset, oneThird + 1 + offset};
 
-    cells[6].spritePosition = olc::vi2d{1, twoThirds + 2};
-    cells[7].spritePosition = olc::vi2d{oneThird + 2, twoThirds + 2};
-    cells[8].spritePosition = olc::vi2d{twoThirds + 2, twoThirds + 2};
+    cells[6].spritePosition = olc::vi2d{offset, twoThirds + 2 + offset};
+    cells[7].spritePosition = olc::vi2d{oneThird + 1 + offset, twoThirds + 2 + offset};
+    cells[8].spritePosition = olc::vi2d{twoThirds + 2 + offset, twoThirds + 2 + offset};
 
     for (cell& cell : cells)
     {
-      cell.content = cellContent{NOT_SET};
+      cell.content = NOT_SET;
     }
 
+    cells[2].content = CROSS;
+    cells[4].content = CIRCLE;
+    cells[8].content = CROSS;
+
     Clear(olc::VERY_DARK_CYAN);
+
+    player1Sprite = NOT_SET;
+
+    mode = PvE;
 
     return true;
   }
@@ -88,34 +110,80 @@ public:
 
     mouse = {GetMouseX(), GetMouseY()};
 
-    // Dawing the playing field
-    DrawRect(0, 0, height, height);
-    DrawLine(oneThird + 1, 1, oneThird + 1, height - 1);
-    DrawLine(twoThirds + 1, 1, twoThirds + 1, height - 1);
-    DrawLine(1, oneThird + 1, height - 1, oneThird + 1);
-    DrawLine(1, twoThirds + 1, height - 1, twoThirds + 1);
-
-    DrawStringProp(height + 10, 5, "Hello World");
-
-    // Drawing the crosses and circles
-    SetPixelMode(olc::Pixel::ALPHA);
-    for (int i = 0; i < 9; i++)
-    {
-      DrawSprite(cells[i].spritePosition, cross);
-    }
-    SetPixelMode(olc::Pixel::NORMAL);
+    PaintPlayingField();
+    PaintUI();
+    PaintSprites();
 
     return true;
   }
 
 private:
+  void PaintPlayingField()
+  {
+    // Dawing the playing field
+    DrawRect(0, 0, height, height, olc::GREY);
+    DrawLine(oneThird + 1, 1, oneThird + 1, height - 1, olc::GREY);
+    DrawLine(twoThirds + 1, 1, twoThirds + 1, height - 1, olc::GREY);
+    DrawLine(1, oneThird + 1, height - 1, oneThird + 1, olc::GREY);
+    DrawLine(1, twoThirds + 1, height - 1, twoThirds + 1, olc::GREY);
+  }
+
+  void PaintUI()
+  {
+    DrawRect(height + 10, 5, width - (height + 20), 15, olc::GREY);
+    DrawLine(width - ((width - height) / 2), 6, width - ((width - height) / 2), 19, olc::GREY);
+
+    switch (mode)
+    {
+      case PvE:
+        FillRect(height + 11, 6, 28, 14, olc::DARK_CYAN);
+      break;
+
+      case PvP:
+        FillRect(height + 11 + 28 + 1, 6, 28, 14, olc::DARK_CYAN);
+      break;
+    }
+
+    DrawStringProp(height + 14, 9, "PvE", (mode == PvE ? olc::WHITE : olc::GREY));
+    DrawStringProp(height + 43, 9, "PvP", (mode == PvP ? olc::WHITE : olc::GREY));
+  }
+
+  void PaintSprites()
+  {
+    // Drawing the crosses and circles
+    SetPixelMode(olc::Pixel::ALPHA);
+
+    for (const cell& cell : cells)
+    {
+      switch (cell.content)
+      {
+        case CROSS:
+          DrawSprite(cell.spritePosition, cross);
+        break;
+
+        case CIRCLE:
+          DrawSprite(cell.spritePosition, circle);
+        break;
+
+        default:
+        break;
+      }
+    }
+
+    SetPixelMode(olc::Pixel::NORMAL);
+  }
 };
 
 int main()
 {
   PGE_tic_tac_toe demo;
 
-  if (demo.Construct(300, 200, 2, 2))
+  // Suitable y heights are 94 + (6 * n) where n is any whole number
+  int n = 10;
+  int y = 94 + (6 * n);
+  int x = y / 2 * 3;
+
+  if (demo.Construct(x, y, 2, 2))
   {
     demo.Start();
   }
