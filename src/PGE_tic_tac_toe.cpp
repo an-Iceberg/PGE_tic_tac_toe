d5967ed1beb26336498d6d3c1532eccbf0d740e0
an-Iceberg <a.g.marosi@bluewin.ch>
2022-07-22 17:03:38 +0200

    implemented mode switching

--- src/PGE_tic_tac_toe.cpp
+++ src/PGE_tic_tac_toe.cpp
@@ -37,13 +37,17 @@ class PGE_tic_tac_toe : public olc::PixelGameEngine
 private:
   olc::Sprite *cross;
   olc::Sprite *circle;
+  olc::Sprite *crossHover;
+  olc::Sprite *circleHover;
   int width;
   int height;
   int oneThird; // The distance for one third of the grid or the position of the second cell from the top left of the grid
   int twoThirds;
   int offset; // This makes the cross and circle centered
   cellContent player1Sprite;
+  cellContent player2Sprite;
   playMode mode;
+  bool playerNeedsToChooseSprite;
   olc::vi2d mouse;
   cell cells [9];
 
@@ -73,6 +77,8 @@ public:
     // The sprites are 32x32px
     cross = new olc::Sprite("./sprites/cross.png");
     circle = new olc::Sprite("./sprites/circle.png");
+    crossHover = new olc::Sprite("./sprites/cross_hover.png");
+    circleHover = new olc::Sprite("./sprites/circle_hover.png");
 
     cells[0].spritePosition = olc::vi2d{offset, offset};
     cells[1].spritePosition = olc::vi2d{oneThird + 1 + offset, offset};
@@ -101,6 +107,8 @@ public:
 
     mode = PvE;
 
+    playerNeedsToChooseSprite = false;
+
     return true;
   }
 
@@ -110,14 +118,75 @@ public:
 
     mouse = {GetMouseX(), GetMouseY()};
 
+    // Only allow user input everywhere if the player does not need to choose a sprite
+    if (!playerNeedsToChooseSprite)
+    {
+      HandleModeChange();
+    }
+
     PaintPlayingField();
     PaintUI();
     PaintSprites();
+
+    // TODO: upon switching mode, a pop-up window should appear asking the player (or player 1 to choose their sprite)
+    if (playerNeedsToChooseSprite)
+    {
+      PaintSpriteChoice();
+    }
 
     return true;
   }
 
 private:
+  void HandleModeChange()
+  {
+    // If a play mode is active only the other one is selectable
+    switch (mode)
+    {
+      case PvE:
+        // Highlight PvP field on hover
+        if (mouse.x > height + 40 && mouse.x < height + 68 && mouse.y > 5 && mouse.y < 20)
+        {
+          FillRect(height + 40, 6, 28, 14, olc::DARK_MAGENTA);
+
+          // Switch to the mode clicked on
+          if (GetMouse(0).bPressed)
+          {
+            mode = PvP;
+
+            playerNeedsToChooseSprite = true;
+
+            for (cell& cell : cells)
+            {
+              cell.content = NOT_SET;
+            }
+          }
+        }
+      break;
+
+      case PvP:
+        // Highlight PvE field on hover
+        if (mouse.x > height + 11 && mouse.x < height + 39 && mouse.y > 5 && mouse.y < 20)
+        {
+          FillRect(height + 11, 6, 28, 14, olc::DARK_MAGENTA);
+
+          // Switch to the mode clicked on
+          if (GetMouse(0).bPressed)
+          {
+            mode = PvE;
+
+            playerNeedsToChooseSprite = true;
+
+            for (cell& cell : cells)
+            {
+              cell.content = NOT_SET;
+            }
+          }
+        }
+      break;
+    }
+  }
+
   void PaintPlayingField()
   {
     // Dawing the playing field
@@ -130,6 +199,7 @@ private:
 
   void PaintUI()
   {
+    // Drawing mode switcher (mode switcher is always present)
     DrawRect(height + 10, 5, width - (height + 20), 15, olc::GREY);
     DrawLine(width - ((width - height) / 2), 6, width - ((width - height) / 2), 19, olc::GREY);
 
@@ -140,12 +210,26 @@ private:
       break;
 
       case PvP:
-        FillRect(height + 11 + 28 + 1, 6, 28, 14, olc::DARK_CYAN);
+        FillRect(height + 40, 6, 28, 14, olc::DARK_CYAN);
       break;
     }
 
     DrawStringProp(height + 14, 9, "PvE", (mode == PvE ? olc::WHITE : olc::GREY));
     DrawStringProp(height + 43, 9, "PvP", (mode == PvP ? olc::WHITE : olc::GREY));
+
+    // TODO: add reset button
+
+    // All other UI elements depend on the mode selected
+    switch (mode)
+    {
+      case PvE:
+        // TODO: difficulty selector between easy (random) or hard (pre-calculated play)
+      break;
+
+      case PvP:
+        // TODO: show, who's turn it is
+      break;
+    }
   }
 
   void PaintSprites()
@@ -171,6 +255,74 @@ private:
     }
 
     SetPixelMode(olc::Pixel::NORMAL);
+  }
+
+  // Paints a pop-up dialogue, allowing the player to choose their preferred sprite
+  void PaintSpriteChoice()
+  {
+    int shadowOffset = 3;
+    int spriteOffset = 15;
+
+    // Shadow
+    SetPixelMode(olc::Pixel::ALPHA);
+    FillRect((oneThird / 2) + shadowOffset, (oneThird / 2) + shadowOffset, (width - oneThird) + shadowOffset, (twoThirds) + shadowOffset, olc::Pixel(0, 0, 0, 128));
+    SetPixelMode(olc::Pixel::NORMAL);
+
+    FillRect(oneThird / 2, oneThird / 2, width - oneThird, twoThirds, olc::VERY_DARK_CYAN);
+
+    // Border
+    DrawRect(oneThird / 2, oneThird / 2, width - oneThird, twoThirds, olc::DARK_CYAN);
+
+    // Text
+    DrawStringProp(oneThird - 4, oneThird - 10, "Please choose a sprite:");
+
+    // Sprites
+    SetPixelMode(olc::Pixel::ALPHA);
+    DrawSprite(oneThird + spriteOffset, oneThird + (oneThird / 3), circle);
+    DrawSprite(height - spriteOffset, oneThird + (oneThird / 3), cross);
+    SetPixelMode(olc::Pixel::NORMAL);
+
+    // Hover over circle
+    if (
+      mouse.x > oneThird + spriteOffset &&
+      mouse.y > oneThird + (oneThird / 3) &&
+      mouse.x < (oneThird + spriteOffset) + 32 &&
+      mouse.y < (oneThird + (oneThird / 3)) + 32)
+    {
+      SetPixelMode(olc::Pixel::ALPHA);
+      DrawSprite(oneThird + spriteOffset, oneThird + (oneThird / 3), circleHover);
+      SetPixelMode(olc::Pixel::NORMAL);
+
+      // Player has selected cricle
+      if (GetMouse(0).bPressed)
+      {
+        player1Sprite = CIRCLE;
+        player2Sprite = CROSS;
+
+        playerNeedsToChooseSprite = false;
+      }
+    }
+
+    // Hover over cross
+    if (
+      mouse.x > height - spriteOffset &&
+      mouse.y > oneThird + (oneThird / 3) &&
+      mouse.x < (height - spriteOffset) + 32 &&
+      mouse.y < (oneThird + (oneThird / 3)) + 32)
+    {
+      SetPixelMode(olc::Pixel::ALPHA);
+      DrawSprite(height - spriteOffset, oneThird + (oneThird / 3), crossHover);
+      SetPixelMode(olc::Pixel::NORMAL);
+
+      // Player has selected cross
+      if (GetMouse(0).bPressed)
+      {
+        player1Sprite = CROSS;
+        player2Sprite = CIRCLE;
+
+        playerNeedsToChooseSprite = false;
+      }
+    }
   }
 };
 
